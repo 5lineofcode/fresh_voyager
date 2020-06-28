8510fe9912910a339cb5a767fdddcaaffe0ad687
Deniansyah <5lineofcode@gmail.com>
2020-06-28 14:31:11 +0700

    Add Database.sql

---
+++ express_pos_db.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost connection
+ Source Server Type    : MySQL
+ Source Server Version : 100411
+ Source Host           : localhost:3306
+ Source Schema         : express_pos_db
+
+ Target Server Type    : MySQL
+ Target Server Version : 100411
+ File Encoding         : 65001
+
+ Date: 28/06/2020 14:30:29
+*/
+
